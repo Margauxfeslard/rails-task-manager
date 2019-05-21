8089d95a4dd3959b16af867fd0c0d448a77496de
Margaux FESLARD margaux.feslard@gmail.com <margaux.feslard@gmail.com>
2019-05-21 15:44:34 +0200

    Tasks

---
+++ app/assets/javascripts/channels/index.js
@@ -0,0 +1,12 @@
+const displayAlertOnButtonClick = () => {
+  const btn = document.querySelector("button");
+  btn.addEventListener("click", (event) => {
+    alert("Thank you!");
+  });
+  // TODO: Select the big green button
+
+  // TODO: Bind the `click` event to the button
+  // TODO: On click, display `Thank you!` in a JavaScript alert!
+};
+
+displayAlertOnButtonClick(); // Do not remove!
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,47 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params_task)
+    @task.completed = params[:task][:completed].nil? ? false : true
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(@task)
+  end
+
+  private
+
+  def params_task
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
